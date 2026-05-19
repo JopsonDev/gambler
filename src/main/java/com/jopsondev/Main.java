f9e661dbbe9d0ef65e00890be5c3326f8497a34b
JopsonDev <jopsonmason@outlook.com>
2026-05-19 16:16:18 -0400

    adjust main back to normal

--- src/main/java/com/jopsondev/Main.java
+++ src/main/java/com/jopsondev/Main.java
@@ -7,37 +7,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-       /* UserInterface system = new UserInterface();
-        system.display();*/
+        UserInterface system = new UserInterface();
+        system.display();
 
-        Slot slot = new Slot();
-        slot.reels();
+        /*Slot slot = new Slot();
+        slot.reels();*/
         //add slot display have it print different symbols before landing on correct one
         //add bonus spins
         //switch the switch case to if state to have better probability spins DONE
         // maybe make a slot machine with askey art DONE
-
-
-        //                         _________
-        //                     ___/=========\\___
-        //                   /  o  o  o  o  o   \\
-        //                  /_____________________\\
-        //                  ||   SLOT MACHINE     ||
-        //          ________||====================||________
-        //        /      o   o   o   o   o   o    o       \\
-        //       /_________________________________________\\
-        //      |      __________ ___________ __________    |
-        //      |     |          |           |          |   |
-        //      |     |    7     |     7     |     7    |   |
-        //      |     |          |           |          |   |
-        //      |     |__________|___________|__________|   |
-        //      |    ___________________________________    |
-        //      |  /_____________________________________\\\\
-        //      |  |       ___       ___       ___         ||
-        //      |  |      |___|     |___|     |___|        ||
-        //      |  |                                  ()   ||
-        //      |  |_______________________________________||
-        //      |/__________________________________________|
 
     }
 }
